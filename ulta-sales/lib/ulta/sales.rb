c862eec344745e1cd32ce9bd5fcd5e3eaada4eb7
Donna Amos <donnacamos.developer@gmail.com>
2019-04-16 02:44:29 +0000

    modification README

---
+++ ulta-sales/lib/ulta/sales.rb
@@ -0,0 +1,8 @@
+require "ulta/sales/version"
+
+module Ulta
+  module Sales
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ ulta-sales/lib/ulta/sales/version.rb
@@ -0,0 +1,5 @@
+module Ulta
+  module Sales
+    VERSION = "0.1.0"
+  end
+end
