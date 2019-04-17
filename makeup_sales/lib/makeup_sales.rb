07c66a17833fbb752610d0b521705064e2898676
Donna Amos <donnacamos.developer@gmail.com>
2019-04-17 15:19:50 +0000

    new /bin file makeup_sales

---
+++ makeup_sales/lib/makeup_sales.rb
@@ -0,0 +1,6 @@
+require "makeup_sales/version"
+
+module MakeupSales
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ makeup_sales/lib/makeup_sales/version.rb
@@ -0,0 +1,3 @@
+module MakeupSales
+  VERSION = "0.1.0"
+end
