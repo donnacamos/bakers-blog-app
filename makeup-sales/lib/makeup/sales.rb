dee83c2510b3e896b42726bad2f0840f3b688b02
Donna Amos <donnacamos.developer@gmail.com>
2019-05-01 01:17:54 +0000

    bin refactor

--- makeup-sales/lib/makeup/sales.rb
+++
@@ -1,8 +0,0 @@
-require "makeup/sales/version"
-
-module Makeup
-  module Sales
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- makeup_sales/lib/makeup_sales.rb
+++
@@ -1,6 +0,0 @@
-require "makeup_sales/version"
-
-module MakeupSales
-  class Error < StandardError; end
-  # Your code goes here...
-end
