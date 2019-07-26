84323870eab7ad60b5d2e3a4e2061a1ad2551bf3
Donna Amos <donnacamos.developer@gmail.com>
2019-07-26 18:56:47 +0000

    modified blogs_controller and posts/new.erb files

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,3 +1,25 @@
-class Blogs < ApplicationController::Base 
+class Blogs < ApplicationController 
   
+  # get posts/new 
+  
+   get '/posts/new' do 
+     erb :'/posts/new' 
+   end 
+   
+   post '/posts' do 
+    raise params.inspect 
+    
+    
+    if !logged_in
+      redirect to '/'
+    end 
+    
+    if params[:posts] != ""
+      @post = Post.create(posts: params[:posts], recipes: params[:recipes], user_id: current_user.id) 
+      redirect to '/posts/#{@post.id}' 
+    else 
+      
+    end 
+   end
+   
 end 
--- config.ru
+++ config.ru
@@ -4,6 +4,5 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use BlogsController 
 use UsersController 
 run ApplicationController
